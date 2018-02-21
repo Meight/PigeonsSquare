b86f4e868dbbd815f40991055bfc1886ec956a55
matt.leboucher <matt.leboucher@gmail.com>
2018-02-21 13:41:30 -0500

    Creates food model.

---
+++ src/Model/Food.java
@@ -0,0 +1,16 @@
+package Model;
+
+/**
+ * @author Matthieu Le Boucher
+ */
+public class Food {
+    private boolean isFresh = true;
+
+    public boolean isFresh() {
+        return isFresh;
+    }
+
+    public void setFresh(boolean fresh) {
+        isFresh = fresh;
+    }
+}
