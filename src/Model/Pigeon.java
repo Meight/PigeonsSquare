46964bd73cc9660e1c6071e55ad51057842302c3
matt.leboucher <matt.leboucher@gmail.com>
2018-02-21 13:40:09 -0500

    Adds position information to pigeons.

--- src/Model/Pigeon.java
+++ src/Model/Pigeon.java
@@ -4,6 +4,15 @@ package Model;
  * Created by lefou on 20/02/2018.
  */
 public abstract class Pigeon extends Thread {
+    /*
+     * The x coordinate of the pigeon on the screen.
+     */
+    public int x;
+
+    /*
+     * The y coordinate of the pigeon on the screen.
+     */
+    public int y;
 
     private double _vitesse;
 
