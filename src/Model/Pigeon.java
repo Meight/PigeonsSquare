8930ee0372ef164f3b3c4df393db05562a0f58c1
matt.leboucher <matt.leboucher@gmail.com>
2018-02-21 13:56:05 -0500

    Refactors pigeon model.

--- src/Model/Pigeon.java
+++ src/Model/Pigeon.java
@@ -2,19 +2,23 @@ package Model;
 
 /**
  * Created by lefou on 20/02/2018.
+ * @author Matthieu Le Boucher
  */
 public abstract class Pigeon extends Thread {
-    /*
+    /**
      * The x coordinate of the pigeon on the screen.
      */
     public int x;
 
-    /*
+    /**
      * The y coordinate of the pigeon on the screen.
      */
     public int y;
 
-    private double _vitesse;
+    /**
+     * The movement speed of the pigeon.
+     */
+    private double speed;
 
     public Pigeon(int x, int y) {
         this.x = x;
