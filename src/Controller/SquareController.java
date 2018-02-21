30e3a839ab064b4a6b4cfcd23857b1584eda6a79
matt.leboucher <matt.leboucher@gmail.com>
2018-02-21 13:48:33 -0500

    Creates square controller.

---
+++ src/Controller/SquareController.java
@@ -0,0 +1,24 @@
+package Controller;
+
+import Model.Ramier;
+import Model.Square;
+
+/**
+ * @author : Matthieu Le Boucher
+ */
+public class SquareController {
+    private Square square;
+
+    public void createSquare() {
+        square = new Square(600, 400);
+        Ramier myBeautifulRamier = new Ramier();
+    }
+
+    public void updateSquare() {
+
+    }
+
+    public void renderSquare() {
+
+    }
+}
