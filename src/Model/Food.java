e1117ac05e7e30efbeadbea11e3849ccc604282f
matt.leboucher <matt.leboucher@gmail.com>
2018-02-21 13:49:32 -0500

    Food now has position.

--- src/Model/Food.java
+++ src/Model/Food.java
@@ -4,7 +4,22 @@ package Model;
  * @author Matthieu Le Boucher
  */
 public class Food {
+    public int x;
+
+    public int y;
+
     private boolean isFresh = true;
+
+    public Food(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Food(int x, int y, boolean isFresh) {
+        this.x = x;
+        this.y = y;
+        this.isFresh = isFresh;
+    }
 
     public boolean isFresh() {
         return isFresh;
